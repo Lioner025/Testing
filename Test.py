a3ad9129e249276aa42803daecc37b68a03e53aa
Lioner025 <87209354+Lioner025@users.noreply.github.com>
2022-01-23 14:17:49 -0400

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,1 +1,13 @@
+rom urllib import request
+import requests
+from bs4 import BeautifulSoup
 
+def search(url):
+    main_path = 'https://www.ytdownload.cloud/en6/'
+    req = requests.get(main_path)
+    soup = BeautifulSoup(req.text,'htmlparser')
+    main_link = 'https://www.ytdownload.cloud/en6/conversion/',
+    search_url = url
+
+
+#https://www.ytdownload.cloud/en6/sdownload/k4OO_NA_gUCFVOPWTVkioLK7R0bx9tIX6U6Y7lwUVsQ/
